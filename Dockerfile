06ae98e772668a1eced3dc16d3b63697591687a8
Kacper Madejczyk <kacper.madejczyk@pollub.edu.pl>
2024-01-13 17:47:02 +0100

    first commit

---
+++ Dockerfile
@@ -0,0 +1,6 @@
+FROM php:8.2-apache-bookworm
+ARG APP_VERSION
+WORKDIR /var/www/html
+COPY app.php index.php
+RUN echo "SetEnv APP_VERSION ${APP_VERSION}" >> /etc/apache2/conf-enabled/environment.conf
+EXPOSE 80
---
+++ app.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html lang="pl">
+    <head>
+        <meta charset="utf-8" />
+        <title>Zadanie 2 - chmury</title>
+    </head>
+    <body>
+        <h3>Zadanie 2 - Programowanie fullstack w chmurze obliczeniowej</h3>
+        <h3>Autor: Kacper Madejczyk</h3>
+        <h3>Wersja: <?php echo getenv('APP_VERSION') ?></h3>
+    </body>
+</html>
